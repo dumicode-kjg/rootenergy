428a2dcb8f7d54a629422e42dd58e660f094f6af
두미코드 <dumicode@kakao.com>
2023-07-11 17:11:06 +0900

    추가작업진행 가이드

--- js/design.js
+++ js/design.js
@@ -1,3 +1,33 @@
+// Tab
+var tabSwiper;
 $(function () {
-  //
+  if ($(".tab_swiper").length) {
+    tabSwiper = new Swiper(".tab_swiper", {
+      slidesPerView: "auto",
+      spaceBetween: 24,
+      observer: true,
+      observeParents: true,
+    });
+  }
+});
+function tabGo(index) {
+  tabSwiper.slideTo(index);
+  $(".tab_swiper a").removeClass("active").eq(index).addClass("active");
+}
+
+// input
+$(function () {
+  $("input.text").each(function () {
+    if ($(this).val() == "" || $(this).val() == undefined) {
+      $(this).removeClass("wr");
+    } else {
+      $(this).addClass("wr");
+    }
+  });
+
+  $("input.text")
+    .on("input change", function () {
+      $(this).toggleClass("wr", Boolean($(this).val()));
+    })
+    .trigger("propertychange");
 });
